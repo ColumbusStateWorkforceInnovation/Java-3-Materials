b7dec5159bda9c9ac71a084b6c4db7aadb3aa52a
Jim Kirkbride <jkirkbride@cscc.edu>
2020-08-30 13:59:03 -0400

    Completed solution.

---
+++ src/edu/cscc/xml/Customer.java
@@ -0,0 +1,45 @@
+package edu.cscc.xml;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+import java.io.Serializable;
+
+@XmlRootElement(name = "Customer")
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Customer implements Serializable {
+    private String firstName;
+    private String lastName;
+
+    public Customer() {
+    }
+
+    public Customer(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}

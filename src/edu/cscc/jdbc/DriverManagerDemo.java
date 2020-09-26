6284465578e08257d08967b552334ee1180d1976
Jim Kirkbride <jkirkbride@cscc.edu>
2020-09-26 19:06:27 -0400

    Adding DriverManager example.

---
+++ src/edu/cscc/jdbc/DriverManagerDemo.java
@@ -0,0 +1,26 @@
+package edu.cscc.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DriverManagerDemo {
+    private static final String MYSQL_DB_USERNAME = "user";
+    private static final String MYSQL_DB_PASSWORD = "password";
+
+    public static void main(String[] args) {
+        try {
+            Properties properties = new Properties();
+            properties.setProperty(MYSQL_DB_USERNAME, "root");
+            properties.setProperty(MYSQL_DB_PASSWORD, "password");
+            String dbUrl = "jdbc:mysql://localhost:3306/java_3_db";
+            Connection connection = DriverManager.getConnection(dbUrl, properties);
+
+            System.out.println("Connection is open: " + !connection.isClosed());
+            connection.close();
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+}

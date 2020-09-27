55a3636ce001ec9964f1fa13ed85b3118c8cb7e6
Jim Kirkbride <jkirkbride@cscc.edu>
2020-09-27 15:38:09 -0400

    Adding Java classes to map tables to objects.

---
+++ src/edu/cscc/jdbc/exercise/Company.java
@@ -0,0 +1,40 @@
+package edu.cscc.jdbc.exercise;
+
+public class Company {
+
+    private Integer id;
+    private String name;
+
+    public Company(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Company(String name) {
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
---
+++ src/edu/cscc/jdbc/exercise/InsuredMember.java
@@ -0,0 +1,63 @@
+package edu.cscc.jdbc.exercise;
+
+public class InsuredMember {
+    private Integer id;
+    private Integer companyId;
+    private String firstName;
+    private String lastName;
+
+    public InsuredMember(Integer companyId, String firstName, String lastName) {
+        this.companyId = companyId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public InsuredMember(Integer id, Integer companyId, String firstName, String lastName) {
+        this.id = id;
+        this.companyId = companyId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getCompanyId() {
+        return companyId;
+    }
+
+    public void setCompanyId(Integer companyId) {
+        this.companyId = companyId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "InsuredMember{" +
+                "id=" + id +
+                ", companyId=" + companyId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                '}';
+    }
+}

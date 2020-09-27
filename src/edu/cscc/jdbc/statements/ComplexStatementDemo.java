a89b7bbeb204e9bdf0e7d3d88f1205730363573f
Jim Kirkbride <jkirkbride@cscc.edu>
2020-09-27 06:53:39 -0400

    Splitting statement demos up. Moved into their own package.

---
+++ src/edu/cscc/jdbc/statements/ComplexStatementDemo.java
@@ -0,0 +1,46 @@
+package edu.cscc.jdbc.statements;
+
+import edu.cscc.jdbc.Java3DBFactory;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+public class ComplexStatementDemo {
+    public static void main(String[] args) {
+        DataSource dataSource = Java3DBFactory.buildDataSource();
+        Connection connection = null;
+
+        try {
+            connection = dataSource.getConnection();
+            Statement statement = connection.createStatement();
+
+            String query =
+                    "select c.name, im.first_name, im.last_name"
+                            + " from companies c "
+                            + " inner join insured_members im on c.id = im.company_id";
+
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String companyName = resultSet.getString("name");
+                String firstName = resultSet.getString("first_name");
+                String lastName = resultSet.getString("last_name");
+                System.out.println("Company: " + "name: " + companyName);
+                System.out.println(firstName + " " + lastName);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    private static void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();;
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
--- src/edu/cscc/jdbc/statements/StatementDemo.java
+++ src/edu/cscc/jdbc/statements/StatementDemo.java
@@ -1,4 +1,6 @@
-package edu.cscc.jdbc;
+package edu.cscc.jdbc.statements;
+
+import edu.cscc.jdbc.Java3DBFactory;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -9,15 +11,28 @@ import java.sql.Statement;
 public class StatementDemo {
     public static void main(String[] args) {
         DataSource dataSource = Java3DBFactory.buildDataSource();
+        Connection connection = null;
 
         try {
-            Connection connection = dataSource.getConnection();
+            connection = dataSource.getConnection();
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery("Select c.id, c.name from companies c");
             while (resultSet.next()) {
-                int id = resultSet.getInt("id");
-                String name = resultSet.getString("name");
+                int id = resultSet.getInt(1);
+                String name = resultSet.getString(2);
                 System.out.println("Company: " + "id: " + id + ", name: " + name);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        } finally {
+            closeConnection(connection);
+        }
+    }
+
+    private static void closeConnection(Connection connection) {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();;
             }
         } catch (SQLException exception) {
             exception.printStackTrace();

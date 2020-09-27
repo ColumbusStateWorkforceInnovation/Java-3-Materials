bc616f7c32e5d1df12db6c947fb2e992f151431e
Jim Kirkbride <jkirkbride@cscc.edu>
2020-09-27 08:51:14 -0400

    Refactoring.

--- src/edu/cscc/jdbc/statements/modifyingdata/ExecuteUpdateDemo.java
+++ src/edu/cscc/jdbc/statements/modifyingdata/ExecuteUpdateDemo.java
@@ -17,40 +17,16 @@ public class ExecuteUpdateDemo {
             System.out.println("Before inserts:");
             findCompanies(connection);
 
-            System.out.println("Inserting McDonald's and Wendy's...");
-            String insertSqlStatement = "insert into companies (name) values(?)";
-            String companyName = "McDonald's";
-            PreparedStatement preparedStatement = connection.prepareStatement(insertSqlStatement);
-            preparedStatement.setString(1, companyName);
-            int result = preparedStatement.executeUpdate();
-            System.out.println("Number of records inserted: " + result);
-
-            preparedStatement.setString(1, "Wendy's");
-            result = preparedStatement.executeUpdate();
-            System.out.println("Number of records inserted: " + result);
-            System.out.println();
-
+            insertRecords(connection);
             System.out.println("After inserts:");
             findCompanies(connection);
 
-            System.out.println("Updating McDonald's to Burger King...");
-            String updateSql =
-                    "update companies c set c.name = ? where c.name = ?";
-            PreparedStatement updateStatement = connection.prepareStatement(updateSql);
-            updateStatement.setString(1, "Burger King");
-            updateStatement.setString(2, "McDonald's");
-            updateStatement.executeUpdate();
+            updateRecords(connection);
             System.out.println("After updating...");
             findCompanies(connection);
 
             System.out.println("Deleting Burger King and Wendy's...");
-            String deleteSql =
-                    "delete from companies where name = ?";
-            PreparedStatement deleteStatement = connection.prepareStatement(deleteSql);
-            deleteStatement.setString(1, "Burger King");
-            deleteStatement.executeUpdate();
-            deleteStatement.setString(1, "Wendy's");
-            deleteStatement.executeUpdate();
+            deleteRecords(connection);
 
             findCompanies(connection);
         } catch (SQLException exception) {
@@ -58,6 +34,41 @@ public class ExecuteUpdateDemo {
         } finally {
             closeConnection(connection);
         }
+    }
+
+    private static void deleteRecords(Connection connection) throws SQLException {
+        String deleteSql =
+                "delete from companies where name = ?";
+        PreparedStatement deleteStatement = connection.prepareStatement(deleteSql);
+        deleteStatement.setString(1, "Burger King");
+        deleteStatement.executeUpdate();
+        deleteStatement.setString(1, "Wendy's");
+        deleteStatement.executeUpdate();
+    }
+
+    private static void updateRecords(Connection connection) throws SQLException {
+        System.out.println("Updating McDonald's to Burger King...");
+        String updateSql =
+                "update companies c set c.name = ? where c.name = ?";
+        PreparedStatement updateStatement = connection.prepareStatement(updateSql);
+        updateStatement.setString(1, "Burger King");
+        updateStatement.setString(2, "McDonald's");
+        updateStatement.executeUpdate();
+    }
+
+    private static void insertRecords(Connection connection) throws SQLException {
+        System.out.println("Inserting McDonald's and Wendy's...");
+        String insertSqlStatement = "insert into companies (name) values(?)";
+        String companyName = "McDonald's";
+        PreparedStatement preparedStatement = connection.prepareStatement(insertSqlStatement);
+        preparedStatement.setString(1, companyName);
+        int result = preparedStatement.executeUpdate();
+        System.out.println("Number of records inserted: " + result);
+
+        preparedStatement.setString(1, "Wendy's");
+        result = preparedStatement.executeUpdate();
+        System.out.println("Number of records inserted: " + result);
+        System.out.println();
     }
 
     private static void findCompanies(Connection connection) throws SQLException {
